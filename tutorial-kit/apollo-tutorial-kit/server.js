02dc9dddfb052bc3a59b8f45839a8df1ef71dbcb
Nicolas-Gras <nicolas.gras@desjardins.com>
2018-02-02 14:41:36 -0500

    Add Apollo Engine to server (go to https://engine.apollographql.com to get API KEY)

--- tutorial-kit/apollo-tutorial-kit/server.js
+++ tutorial-kit/apollo-tutorial-kit/server.js
@@ -3,11 +3,40 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
 
+import compression from 'compression';  // Add Apollo Engine to server
+import { Engine } from 'apollo-engine'; // ...
+
 const GRAPHQL_PORT = 3000;
+
+const ENGINE_API_KEY = 'service:NicolasGras-7982:B_GqQ3x7lMC3PlyhaUXEgg'; // TODO - Add Apollo Engine to server
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+// Add Apollo Engine to server (Start)
+const engine = new Engine({
+  engineConfig: {
+    apiKey: ENGINE_API_KEY
+  },
+  graphqlPort: GRAPHQL_PORT
+});
+
+engine.start();
+
+graphQLServer.use(engine.expressMiddleware());
+
+graphQLServer.use(compression());
+// Add Apollo Engine to server (End)
+
+graphQLServer.use('/graphql', 
+                    bodyParser.json(), 
+                    graphqlExpress(
+                      { schema,
+                        tracing: true   // Add Apollo Engine to server (This option turns on tracing)
+                      }
+                    )
+                  );
+
+
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
