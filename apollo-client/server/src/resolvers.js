34ff4c60cb3ea421fcd4f2b928234a7941719ccc
Nicolas-Gras <nicolas.gras@desjardins.com>
2018-02-07 13:26:54 -0500

    Add mutation

--- apollo-client/server/src/resolvers.js
+++ apollo-client/server/src/resolvers.js
@@ -1,10 +1,19 @@
-const channels = [{
-    id: 1,
-    name: 'soccer',
-}, {
-    id: 2,
-    name: 'baseball',
-}];
+const channels = [
+    {
+        id: 1,
+        name: 'soccer',
+    }, 
+    {
+        id: 2,
+        name: 'baseball',
+    }, 
+    {
+        id: 3,
+        name: 'handball',
+    }
+];
+
+let nextId=4;
 
 export const resolvers = {
     Query: {
@@ -12,4 +21,18 @@ export const resolvers = {
             return channels;
         },
     },
+    Mutation: {
+        addChannel: (root, args) => {
+            
+            console.log("---------------------------------------");
+            console.log(root);
+            console.log("---------------------------------------");
+            console.log(args);
+            console.log("---------------------------------------");
+            
+            const newChannel = { id: nextId++, name: args.name };
+            channels.push(newChannel);
+            return newChannel;
+        }
+    }
 };
