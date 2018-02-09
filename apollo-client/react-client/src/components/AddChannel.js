70ceb0cba37d3c4d1d138e455a845b3a47eff5f4
Nicolas-Gras <nicolas.gras@desjardins.com>
2018-02-09 16:08:31 -0500

    Add mutation. Page need to be refreshed to see modification.

--- apollo-client/react-client/src/components/AddChannel.js
+++ apollo-client/react-client/src/components/AddChannel.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const AddChannel = () => {
+const AddChannel = (props) => {
     const handleKeyUp = (evt) => {
         if (evt.keyCode === 13) {
           console.log(evt.target.value);
-          evt.target.value = '';
+          evt.persist();
+          props.mutate({ 
+            variables: { name: evt.target.value }
+          })
+          .then( res => {
+            evt.target.value = '';  
+          });
+                    
         }
       };
 
@@ -17,5 +26,14 @@ const AddChannel = () => {
   );
 };
 
+const addChannelMutation = gql`
+  mutation addChannel($name: String!) {
+    addChannel(name: $name) {
+      id
+      name
+    }
+  }
+`;
+const AddChannelWithMutation = graphql(addChannelMutation)(AddChannel);
 
-export default AddChannel;
+export default AddChannelWithMutation;
